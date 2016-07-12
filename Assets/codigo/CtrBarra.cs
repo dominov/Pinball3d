b0d45af6b1534aac2df9baa1a6bafb6315eabfba
Cristian Pardo (MAC) <pardocrespocristian@gmail.com>
2016-07-12 18:19:01 -0500

    dia uno2
    mov de la pelota

---
+++ Assets/codigo/CtrBarra.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CtrBarra : MonoBehaviour {
+	public float velocidad; 
+	private Vector3 posicionInicial;
+	// Use this for initialization
+	void Start () {
+		posicionInicial = this.transform.position;
+	}
+
+	public void ReiniciarPos(){
+		
+	}
+	// Update is called once per frame
+	void Update () {
+		float tecladoHorizontal = Input.GetAxisRaw ("Horizontal");
+		float posX = this.transform.position.x + (tecladoHorizontal * velocidad * Time.deltaTime);
+
+		this.transform.position = new Vector3 (Mathf.Clamp( posX,-8,8), this.transform.position.y, this.transform.position.z);
+	}
+}
---
+++ Assets/codigo/CtrPelota.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CtrPelota : MonoBehaviour {
+
+
+	public Rigidbody rb;// se inicaliza desde el inspiector 
+	public float velocidad;
+	public Transform barra;
+
+
+	bool enJuego;
+	Vector3 posicionInicial;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!enJuego && Input.GetButtonDown ("Fire1")) {
+			enJuego = true;
+			this.transform.SetParent (null);
+			this.rb.isKinematic = false;
+			this.rb.AddForce (new Vector3 (velocidad, velocidad, 0));
+		}
+	}
+
+	public void ReiniciarPos(){
+		this.transform.position =  this.posicionInicial;
+		this.transform.SetParent (barra);
+		enJuego = false;
+		DetenerVelocidad ();
+	}
+
+	public void DetenerVelocidad(){
+		this.rb.isKinematic = true;
+		this.rb.velocity = Vector3.zero;
+	}
+}
